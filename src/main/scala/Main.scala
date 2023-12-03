c29a6bdc2d6905ddadb1aa68ba41ef91046b9a59
Osmanilge <o.unaldi@etu.edu.tr>
2023-12-03 21:58:40 +0300

    sample case created

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,9 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-import org.apache.spark.graphx.{Graph, VertexId, VertexRDD, Edge}
+import org.apache.spark.graphx._
+import org.apache.spark.graphx.Graph.graphToGraphOps
 import org.apache.spark.rdd.RDD
+import scala.reflect.ClassTag
 import java.io.PrintWriter
 object Main extends App {
   
@@ -9,14 +11,14 @@ object Main extends App {
     val sc = new SparkContext(sparkConf)
 
     //Read edges from the CSV file and parse them into Edge objects
-    val edgesRDD: RDD[Edge[Int]] = sc.textFile("src/sampleFeatures.csv")
+    val edgesRDD: RDD[Edge[Int]] = sc.textFile("src/sampleEdges.csv")
       .filter(line => !line.startsWith("numeric_id_1"))
       .map { line =>
         val parts = line.split(",")
         Edge(parts(0).toLong, parts(1).toLong, 1)
       }
 
-    val verticesRDD: RDD[(VertexId, (Int, Int, Long, String, String, Long, Int, String, Int))] = sc.textFile("src/sampleEdges.csv")
+    val verticesRDD: RDD[(VertexId, (Int, Int, Long, String, String, Long, Int, String, Int))] = sc.textFile("src/sampleFeatures.csv")
       .filter(line => !line.startsWith("views"))
       .map { line =>
         val parts = line.split(",")
@@ -37,18 +39,48 @@ object Main extends App {
       }
       
     // Graph'ı oluşturma
-    val graph = Graph(verticesRDD, edgesRDD)
+      val graph = Graph(verticesRDD, edgesRDD)
+      //val graph = Graph.fromEdges(edgesRDD, None)
+  /*// Düğümleri (vertices) yazdırma
+  println("Düğümler (Vertices):")
+  graph.vertices.collect().foreach { case (id, vertex) =>
+    println(s"ID: $id, Vertex: $vertex")
+  }*/
 
-// Düğümleri (vertices) yazdırma
-println("Düğümler (Vertices):")
-graph.vertices.collect().foreach { case (id, vertex) =>
-  println(s"ID: $id, Vertex: $vertex")
-}
+  // Kenarları (edges) yazdırma
+  /*println("Kenarlar (Edges):")
+  graph.edges.collect().foreach { edge =>
+    println(s"Kaynak: ${edge.srcId}, Hedef: ${edge.dstId}, Kenar: ${edge.attr}")
+  }*/
+   
 
-// Kenarları (edges) yazdırma
-println("Kenarlar (Edges):")
-graph.edges.collect().foreach { edge =>
-  println(s"Kaynak: ${edge.srcId}, Hedef: ${edge.dstId}, Kenar: ${edge.attr}")
-}
-    Louvain.sayHello()
+  // Her bir düğüm için iç ve dış dereceleri hesaplayan fonksiyon
+  val nodeDegrees = graph.aggregateMessages[(Long, Long)](
+    triplet => {
+      // İç derece: Kendi düğümün kenar sayısı
+      triplet.sendToSrc((1L, 0L))
+      triplet.sendToDst((0L, 1L))
+    },
+    // İç ve dış dereceleri toplama işlemi
+    (a, b) => (a._1 + b._1, a._2 + b._2)
+  )
+
+  // Her bir düğümün iç ve dış derecelerini yazdırma
+  nodeDegrees.foreach { case (vertexId, (innerDegree, outerDegree)) =>
+    println(s"Düğüm $vertexId'nin iç derecesi: $innerDegree, dış derecesi: $outerDegree")
+  }
+
+   // Inner ve outer edge sayılarını toplamak
+  val nodeIODegree: RDD[(Long, Long)] = nodeDegrees.map { case (vertexId, (innerDegree, outerDegree)) => (vertexId, innerDegree + outerDegree) }//2 ile çarpılabilabilir
+
+  // Her bir düğümün iç ve dış derecelerini yazdırma
+  nodeIODegree.foreach { case (vertexId, degree) =>
+    println(s"Düğüm $vertexId'nin derecesi: $degree")
+  }
+
+  val totalEdges: Long = nodeIODegree.map { case (vertexId, degree) => degree }.sum().toLong
+
+
+  // Sonuçları yazdırmak
+  println(s"Toplam Edge Sayısı: $totalEdges")
 }
