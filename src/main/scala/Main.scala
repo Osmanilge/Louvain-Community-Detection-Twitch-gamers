dc981fc634e8a23d45d642dc0bf828c7ec560249
Osmanilge <o.unaldi@etu.edu.tr>
2023-12-03 23:38:14 +0300

    some function designs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,11 +5,8 @@ import org.apache.spark.graphx.Graph.graphToGraphOps
 import org.apache.spark.rdd.RDD
 import scala.reflect.ClassTag
 import java.io.PrintWriter
-object Main extends App {
-  
-    val sparkConf = new SparkConf().setAppName("GraphFromFile").setMaster("local[*]")
-    val sc = new SparkContext(sparkConf)
-
+object Main{
+  def loadTheGraph(sc: SparkContext): Graph[(Int, Int, Long, String, String, Long, Int, String, Int), Long] = {
     //Read edges from the CSV file and parse them into Edge objects
     val edgesRDD: RDD[Edge[Long]] = sc.textFile("src/large_twitch_edges.csv")
       .filter(line => !line.startsWith("numeric_id_1"))
@@ -37,36 +34,47 @@ object Main extends App {
           )
         )
       }
-      
-    // Graph'ı oluşturma
-      val graph = Graph(verticesRDD, edgesRDD)
-      //val graph = Graph.fromEdges(edgesRDD, None)
-  /*// Düğümleri (vertices) yazdırma
-  println("Düğümler (Vertices):")
-  graph.vertices.collect().foreach { case (id, vertex) =>
-    println(s"ID: $id, Vertex: $vertex")
-  }*/
-
-  // Kenarları (edges) yazdırma
-  /*println("Kenarlar (Edges):")
-  graph.edges.collect().foreach { edge =>
-    println(s"Kaynak: ${edge.srcId}, Hedef: ${edge.dstId}, Kenar: ${edge.attr}")
-  }*/
-   
-
-  val louvainBasedGraph = Louvain.generateLouvainGraph(graph)
+        
+    // Creat regular Graph
+    val graph = Graph(verticesRDD, edgesRDD)
+    return graph
+  }
 
 
-    // Düğümleri (vertices) yazdırma
-  /*println("Düğümler (Vertices):")
-  louvainBasedGraph.vertices.collect().foreach { case (id, vertex) =>
-    println(s"ID: $id, Vertex: $vertex")
-  }*/
+  def main(args: Array[String]) {
+    val sparkConf = new SparkConf().setAppName("GraphFromFile").setMaster("local[*]")
+    val sc = new SparkContext(sparkConf)
 
-  // Kenarları (edges) yazdırma
-  /*println("Kenarlar (Edges):")
-  louvainBasedGraph.edges.collect().foreach { edge =>
-    println(s"Kaynak: ${edge.srcId}, Hedef: ${edge.dstId}, Kenar: ${edge.attr}")
-  }*/
+    val graph = loadTheGraph(sc)
+    /*// Düğümleri (vertices) yazdırma
+    println("Düğümler (Vertices):")
+    graph.vertices.collect().foreach { case (id, vertex) =>
+      println(s"ID: $id, Vertex: $vertex")
+    }*/
 
+    // Kenarları (edges) yazdırma
+    /*println("Kenarlar (Edges):")
+    graph.edges.collect().foreach { edge =>
+      println(s"Kaynak: ${edge.srcId}, Hedef: ${edge.dstId}, Kenar: ${edge.attr}")
+    }*/
+
+    val louvainBasedGraph = Louvain.generateLouvainGraph(graph)
+    val louvainBasedGraphCached = louvainBasedGraph.cache()
+
+    val nodeDegrees = louvainBasedGraphCached.aggregateMessages[(Long, Long)](
+    triplet => {
+      // İç derece: Kendi düğümün kenar sayısı
+      triplet.sendToSrc((1L, 0L))
+      triplet.sendToDst((0L, 1L))
+    },
+    // İç ve dış dereceleri toplama işlemi
+    (a, b) => (a._1 + b._1, a._2 + b._2)
+    )
+    val nodeIODegree: RDD[(Long, Long)] = nodeDegrees.map { case (vertexId, (innerDegree, outerDegree)) => (vertexId, innerDegree + outerDegree) }
+
+    val m: Long = nodeIODegree.map { case (vertexId, degree) => degree }.sum().toLong
+    var mBroadcasted = sc.broadcast(m)
+    println(s"Toplam Edge Sayısı: $m")
+
+  }
 }
